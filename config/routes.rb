c02b432488dccce1219b4faef53e8b1954b8b6c8
Jefrey Bulla <jefrey.bulla@gmail.com>
2018-08-31 16:27:41 -0700

    form fields required

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   resources :users
 
+  get 'login' => 'sessions#new'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
