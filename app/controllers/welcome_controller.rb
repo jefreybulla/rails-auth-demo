f1eea097bee4a0e822a0f8958587d2b5f48f8699
Jefrey Bulla <jefrey.bulla@gmail.com>
2018-08-31 21:06:10 -0700

    deleted comments and added read me info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,5 @@ class WelcomeController < ApplicationController
   def index
     current_user
     require_user
-    #if session[:user_id]
-    #  @user = User.find(session[:user_id])
-    #end
-
   end
 end
